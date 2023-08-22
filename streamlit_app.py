516d98deef2020428163b71b25791391b0636d84
evilins <77074006+evilins@users.noreply.github.com>
2023-08-22 21:08:44 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,18 @@
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
 
+# Set Streamlit app title
+st.title("Simple Streamlit App")
+
+# Generate random data
+data = np.random.randn(100)
+
+# Create a histogram
+st.subheader("Histogram")
+plt.hist(data, bins=20)
+st.pyplot()
+
+# Show random numbers
+st.subheader("Random Numbers")
+st.write(data)
